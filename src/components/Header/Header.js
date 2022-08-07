02c93ca5941fbead13893bb183eedc73760aaedc
IVKrylova <ivk.lis@yandex.ru>
2022-08-07 22:01:18 +0300

    fix: add link in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import iconSkype from '../../images/skype.svg';
 import { header } from '../../utils/content';
 import './Header.css';
@@ -17,9 +18,11 @@ const Header = props => {
           <a className={`header__lang-link ${classModifierEn}`} href="#" onClick={props.clickOnLangEn}>En</a>
         </li>
       </ul>
-      <h1 className="header__title">
-        {props.language === 'Ru' ? header.Ru : header.En}
-      </h1>
+      <Link to="/" className='link'>
+        <h1 className="header__title">
+          {props.language === 'Ru' ? header.Ru : header.En}
+        </h1>
+      </Link>
       <ul className="header__contacts">
         <li>
           <a className="header__contact" href="tel:+79200780106">8 920 078 01 06</a>
